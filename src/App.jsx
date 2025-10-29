432952e1aee46537a8d6ee06eef16a452724bf01
coffee2023 <susig1212@gmail.com>
2025-10-28 22:58:47 -0400

    upload files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from "./components/Dashboard";
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1 className = "title">✨ Search Old Fantasy Books ✨</h1>
+      <Dashboard />
+    </div>
+  );
+}
